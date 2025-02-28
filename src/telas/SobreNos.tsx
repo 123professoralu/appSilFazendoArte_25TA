b21bf98cd9b4fc2e0ac9ead082dfce4137a7653d
1.23 Professora Lu <112901053+123professoralu@users.noreply.github.com>
2025-02-27 22:26:37 -0300

    Tela Sobre Nós
    Tela Sobre Nós.

--- src/telas/SobreNos.tsx
+++ src/telas/SobreNos.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import {StatusBar} from "react-native";
+import {StatusBar, StyleSheet, View, Image} from "react-native";
 
 import Texto from "../componentes/Texto";
 
 export default function SobreNos(){
-    return <>
+    return <View style={estilos.fundo}>
         <StatusBar />
+        <Image source={require('../../assets/logo.png')} style={estilos.logo} resizeMode="contain"/>
         <Texto>Hello world!</Texto>
-    </>
+    </View>
 }
+
+const estilos = StyleSheet.create({
+    fundo: {
+        backgroundColor: "purple",
+    },
+    logo: {
+        width: 300,
+        height: 300,
+        alignSelf: "center",
+    }
+})
 
