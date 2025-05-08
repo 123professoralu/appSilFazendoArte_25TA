b7f25dde93e26aca66ddd0063d849f110134f9ba
1.23 Professora Lu <112901053+123professoralu@users.noreply.github.com>
2025-05-08 19:16:10 -0300

    Tela de Produtos - Modal
    Tela de Produtos - Modal

--- src/telas/Produtos/Item.tsx
+++ src/telas/Produtos/Item.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "react-native-paper";
-import { View } from "react-native";
+import { View, TouchableOpacity, Modal, Image} from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Texto from '../../componentes/Texto';
-import styles from './estilosProdutos'
+import styles from './estilosProdutos';
 
 export default function Item({prod:{id,nome,imagem,descricao}}:any){
+    
+    const [statusModal, acaoAbreFecha] = useState(false)
+    
     return <View>
-            <Card mode='elevated' style={styles.card}> 
-                <Card.Content>
-                    <Texto style={styles.nomeProduto}>{nome}</Texto>
-                </Card.Content>
-                <Card.Cover source={imagem} style={styles.imagem}/>
-            </Card>
+                <Card mode='elevated' style={styles.card}> 
+                    <Card.Content>
+                        <Texto style={styles.nomeProduto}>{nome}</Texto>
+                    </Card.Content>
+                    <Card.Cover source={imagem} style={styles.imagem}/>
+                    <Card.Actions>
+                        <TouchableOpacity style={styles.botao} onPress={()=>acaoAbreFecha(true)}>
+                            <Texto style={styles.textoBotao}>
+                                <Ionicons name="list" size={12} color="white"/> Detalhes
+                            </Texto>
+                        </TouchableOpacity>
+                    </Card.Actions>
+                </Card>
+
+                <Modal animationType="fade" transparent={true} visible={statusModal}>
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modal}>
+                            <Texto style={styles.nomeProduto}>{nome}</Texto>
+                            <Texto style={styles.descProduto}>{descricao}</Texto>
+                            <Image source={imagem} style={styles.imagemModal} resizeMode="contain"/>
+                            <TouchableOpacity onPress={()=>acaoAbreFecha(false)}>
+                                <Ionicons name="close" size={30} color="purple"/>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
            </View>
 }
